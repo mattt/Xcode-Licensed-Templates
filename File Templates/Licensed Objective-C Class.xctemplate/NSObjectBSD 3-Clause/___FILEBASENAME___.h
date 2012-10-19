02ff6621f73fa54e30fd81a393728e0b591af038
André Mathlener <a.mathlener@mdc-international.com>
2012-10-19 12:06:15 +0200

    Added 'Subclass of:' option

--- File Templates/Licensed Objective-C Class.xctemplate/NSObjectBSD 3-Clause/___FILEBASENAME___.h
+++ File Templates/Licensed Objective-C Class.xctemplate/NSObjectBSD 3-Clause/___FILEBASENAME___.h
@@ -25,8 +25,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 // POSSIBILITY OF SUCH DAMAGE.
 
-#import <Foundation/Foundation.h>
+___IMPORTHEADER_licensedSubclass___
 
-@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_licensedSubclass___
 
 @end
